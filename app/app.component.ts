3f6a10adad831f9d7dbe98b26138f1e53a0caea0
manosatvik <manosatvik@gmail.com>
2019-05-07 10:07:58 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { MouseEvent } from '@agm/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  // google maps zoom level
+  zoom: number = 8;
+  
+  // initial center position for the map
+  lat: number = 16.515099;
+  lng: number = 80.632095;
+
+  
+  
+  
+  
+  markers: marker[] = [
+	  {
+		  lat: 16.497861,
+		  lng: 80.653978,
+		  label: 'A',
+		  draggable: false
+	  },
+	  {
+		  lat: 51.373858,
+		  lng: 7.215982,
+		  label: 'B',
+		  draggable: false
+	  },
+	  {
+		  lat: 51.723858,
+		  lng: 7.895982,
+		  label: 'C',
+		  draggable: false
+	  }
+  ]
+}
+
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
